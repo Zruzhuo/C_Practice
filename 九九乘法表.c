4a992c97297a622f58caea7ca1a3fca103a37119
Zruzhuo <1197736628@qq.com>
2019-05-10 13:05:49 +0800

    九九乘法表.c

---
+++ 九九乘法表.c
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//int Mul(int x, int y)
+//{
+//	int m = 0;
+//	for (x = 1; x < 10; x++)
+//	{
+//		for (y = 1; y <= x; y++)
+//		{
+//			m = x * y;
+//			printf("%d*%d=%-2d  ", x, y, m);
+//		}
+//		printf("\n");	
+//	}
+//}
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	Mul(i, j);
+//	system("pause");
+//	return 0;
+//}
