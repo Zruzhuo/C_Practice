6a27f804cbe7d050e50afa827ef9a6f7fcc87f96
Zruzhuo <1197736628@qq.com>
2019-05-18 10:03:06 +0800

    上传代码

---
+++ G.C.D(最大公约数).c.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int x, y, m, i = 0;
+//	printf("请输入两个整数：");
+//	scanf("%d%d", &x, &y);
+//	m = (x < y) ? x: y;
+//	for (i = m; i > 0; i--)
+//	{
+//		if (0 == x % i && 0 == y % i)
+//		{
+//			printf("最大公约数为：%d", i);
+//			break;
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
