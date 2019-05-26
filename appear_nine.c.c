2be442319cc74b6d1f31f88e176b0c763dfb8788
Zruzhuo <1197736628@qq.com>
2019-05-26 21:29:16 +0800

    上传练习

---
+++ appear_nine.c.c
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int i = 0;
+//	int num = 0;
+//	for (i = 1; i < 101; i++)
+//	{
+//		if (9 == i % 10 )
+//		{
+//			num++;
+//			if (9 == i / 10)
+//			{
+//				num++;
+//			}
+//		}
+//	}
+//	printf("1到 100 的所有整数中出现数字9的次数num = %d", num);
+//	system("pause");
+//	return 0;
+//}
---
+++ arr_swap.c.c
@@ -0,0 +1,38 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int arr1[10] = { 0 };
+//	int arr2[10] = { 0 };
+//	int i, n = 0;
+//	printf("请输入数组arr1的元素：");
+//	for (i = 0; i < 4; i++)
+//	{
+//		scanf("%d", &arr1[i]);
+//	}
+//	printf("请输入数组arr2的元素：");
+//	for (i = 0; i < 4; i++)
+//	{
+//		scanf("%d", &arr2[i]);
+//	}
+//	for (i = 0; i < 4 ; i++)
+//	{
+//		n = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = n;
+//	}
+//	printf("\n数组arr1的元素变为：");
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//	}
+//	printf("\n数组arr2的元素变为：");
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//	}
+//	system("pause");
+//	return 0;
+//}
