8b8591ea66e39208f86c865beb25e3c9464c9f89
Zruzhuo <1197736628@qq.com>
2019-05-28 16:58:35 +0800

    上传练习

---
+++ case write_swap.c.c
@@ -0,0 +1,35 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int Change(char a)
+//{
+//	if (a>='a' && a<='z')
+//	{
+//		a = a - 32;
+//		return a;
+//	}
+//	if (a >= 'A'&&a <= 'Z')
+//	{
+//		a = a + 32;
+//		return a;
+//	}
+//	if (a >= '0'&&a <= '9')
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	char a;
+//	char b;
+//	while (1)
+//	{
+//		a = getchar();
+//		b = Change(a);
+//		printf("%c", b);
+//	}
+//	system("pause");
+//	return 0;
+//}
