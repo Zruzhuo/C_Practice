c2bf218d04009381d9db90a560f84485eea94b12
Zruzhuo <1197736628@qq.com>
2019-05-30 08:46:16 +0800

    上传练习

---
+++ digit_sum.c.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int DigitSum(int n)
+//{
+//	if (n > 0)
+//	{
+//		return n % 10 + DigitSum(n / 10);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d",DigitSum(n));
+//	system("pause");
+//	return 0;
+//}
---
+++ fib.c.c
@@ -0,0 +1,30 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int a = 1;
+//	int b = 1;
+//	int c = 0;
+//	scanf("%d", &n);
+//	if (n <= 2)
+//	{
+//		c = 1;
+//	}
+//	else
+//	{
+//		for (i = 3; i <= n; i++)
+//		{
+//			c = a + b;
+//			a = b;
+//			b = c;
+//		}
+//	}
+//	printf("%d\n", c);
+//	system("pause");
+//	return 0;
+//}
