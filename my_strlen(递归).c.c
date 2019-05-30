64782bb829ec23739408722eed3662ddc5109651
Zruzhuo <1197736628@qq.com>
2019-05-30 18:49:26 +0800

    上传练习

---
+++ my_strlen(递归).c.c
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<assert.h>
+//int My_Strlen(char const * str)
+//{
+//	assert(str != NULL);
+//	if (*str == '\0')
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		return 1 + My_Strlen(str+1);
+//	}
+//}
+//int main()
+//{
+//	char* str = "asdbiasfa";
+//	int ret = 0;
+//	ret = My_Strlen(str);
+//	printf("%d", ret);
+//	system("pause");
+//	return 0;
+//}
