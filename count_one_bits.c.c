6aa243986352508038fa0eab6646df5eea69088b
Zruzhuo <1197736628@qq.com>
2019-06-12 16:02:42 +0800

    上传代码

---
+++ count_one_bits.c.c
@@ -0,0 +1,28 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//int count_one_bits(unsigned int value)
+//{
+//	int count = 0;
+//	while (value)
+//	{
+//		if (value % 2 == 1)
+//		{
+//			count++;			
+//		}
+//		value = value / 2;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int value = 0;
+//	int count = 0;
+//	printf("请输入一个正数：");
+//	scanf("%d", &value);
+//	count = count_one_bits(value);
+//	printf("%d", count);
+//	system("pause");
+//	return 0;
+//}
---
+++ dis_bits.c.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int count = 0;
+//	int ret = a ^ b;
+//	while (ret)
+//	{
+//		ret = ret & (ret - 1);
+//		count++;
+//	}
+//	printf("count=%d\n", count);
+//	system("pause");
+//	return 0;
+//}
