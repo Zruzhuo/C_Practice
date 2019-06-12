da02ffe3b030343c4d5231b34757ae296e557467
Zruzhuo <1197736628@qq.com>
2019-06-12 16:03:07 +0800

    上传代码

---
+++ printf_int_bits.c.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//void Print(int x)
+//{
+//	while (x)
+//	{
+//		printf("%d ", x % 10);
+//		x = x / 10;
+//	}
+//}
+//int main()
+//{
+//	int x = 0;
+//	scanf("%d", &x);
+//	Print(x);
+//	system("pause");
+//	return 0;
+//}
