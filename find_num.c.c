4f856f185ea2bf21e26ee31f726cebce4e9cb0fc
Zruzhuo <1197736628@qq.com>
2019-06-15 13:23:46 +0800

    上传代码

---
+++ find_num.c.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int arr[11] = { 1, 3, 3, 8, 1, 6, 8, 6, 11, 9, 9 };
+//	int i = 0;
+//	int size = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 1; i < size; i++)
+//	{
+//		arr[0] = arr[0] ^ arr[i];
+// 	}
+//	printf("%d", arr[0]);
+//	system("pause");
+//	return 0;
+//}
---
+++ mean_value.c.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//int average(int a, int b)
+//{
+//	int ret = 0;
+//	a = a < b ? a : b;
+//	ret = a + (b - a) / 2;
+//	return ret;
+//}
+//int main()
+//{
+//	int a = 0, b = 0;
+//	scanf("%d%d", &a, &b);
+//	printf("%d\n", average(a, b));
+//	system("pause");
+//	return 0;
+//}
