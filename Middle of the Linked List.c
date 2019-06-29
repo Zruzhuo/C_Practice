786da7de804a24aeaf9ea532035cbe3fff2844d3
Zruzhuo <1197736628@qq.com>
2019-06-29 16:09:20 +0800

    提交练习

---
+++ Middle of the Linked List.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* middleNode(struct ListNode* head) {
+	if (head == NULL)
+	{
+		return head;
+	}
+	struct ListNode* fast = head;
+	struct ListNode* slow = head;
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return slow;
+}
