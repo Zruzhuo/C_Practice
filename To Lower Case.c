dce6f49efc1ab9a313ca1268f7eda4c3a8230c53
Zruzhuo <1197736628@qq.com>
2019-08-09 23:37:10 +0800

    上传代码

---
+++ To Lower Case.c
@@ -0,0 +1,15 @@
+
+
+char * toLowerCase(char * str) {
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 'a' - 'A';
+		}
+		++i;
+	}
+	return str;
+}
+
