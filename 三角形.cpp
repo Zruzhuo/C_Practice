a9ed54c0012850ab7f35772cac19bd62962923ca
Zruzhuo <1197736628@qq.com>
2019-12-09 18:01:00 +0800

    上传代码

---
+++ 三角形.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	while (cin >> a >> b >> c) {
+		if (a + b > c || b + c > a || a + c > b)
+			cout << "Yes" << endl;
+		else
+			cout << "No" << endl;
+	}
+	return 0;
+}
