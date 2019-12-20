fcc13624ac570fa3616f2f5c59f4aedf25b0d16c
Zruzhuo <1197736628@qq.com>
2019-12-20 23:51:31 +0800

    上传练习

--- 模拟/模拟/源.cpp
+++ 模拟/模拟/源.cpp
@@ -2,64 +2,64 @@
 #include<string>
 using namespace std;
 
-////#define NoEdge -1   //两点之间无法到达
-////
-////int n = 4;    //顶点（城市）数量
-////int cost = 0; //从出发城市到当前位置的代价
-////int bestc = NoEdge; //最优代价
-////int bestx[4];     //最优解
-////int x[4];         //x[i]记录第i个城市的索引
-////int a[4][4] = {1,30,6,4,30,-1,5,10,6,5,-1,20,4,10,20,-1};//记录城市之间代价，如果两座城市之间不连通则为NoEdge
-////
-////bool isHave(int i, int t)
-////{
-////	for (int j = 0; j < t; j++)
-////		if (x[j] == i)return true;
-////	return false;
-////}
-////
-////void Backtrack(int t)
-////{
-////	if (t == n)
-////	{
-////		//最后一个城市和出发城市是否可到达，该条通路的代价是不是最优
-////		if (a[x[n - 1]][0] != NoEdge
-////			&& ((cost + a[x[n - 1]][0]) < bestc || bestc == NoEdge))
-////		{
-////			for (int i = 0; i < n; i++)
-////			{
-////				bestx[i] = x[i];
-////				bestc = cost + a[x[n - 1]][0];
-////			}
-////		}
-////	}
-////	else
-////	{
-////		for (int i = 0; i < n; i++)
-////		{
-////			//当前城市是否已经走过，当前城市与上一座城市之间是否连通
-////			if (!isHave(i, t) && a[x[t - 1]][i] != NoEdge && ((cost + a[x[t - 1]][i]) < bestc || bestc == NoEdge))
-////			{
-////				cost += a[x[t - 1]][i];
-////				//cout<<t<<endl;
-////				x[t] = i;
-////				Backtrack(t + 1);
-////				cost -= a[x[t - 1]][i];
-////			}
-////		}
-////	}
-////}
-////int main()
-////{
-////	x[0] = 0; //出发城市固定为第一座城市
-////	Backtrack(1);
-////	for (int i = 0; i < n; i++)
-////		cout << bestx[i] + 1 << " ";
-////	cout << endl;
-////	cout << bestc << endl;
-////	system("pause");
-////	return 0;
-////}
+//#define NoEdge -1   //两点之间无法到达
+//
+//int n = 4;    //顶点（城市）数量
+//int cost = 0; //从出发城市到当前位置的代价
+//int bestc = NoEdge; //最优代价
+//int bestx[4];     //最优解
+//int x[4];         //x[i]记录第i个城市的索引
+//int a[4][4] = {1,30,6,4,30,-1,5,10,6,5,-1,20,4,10,20,-1};//记录城市之间代价，如果两座城市之间不连通则为NoEdge
+//
+//bool isHave(int i, int t)
+//{
+//	for (int j = 0; j < t; j++)
+//		if (x[j] == i)return true;
+//	return false;
+//}
+//
+//void Backtrack(int t)
+//{
+//	if (t == n)
+//	{
+//		//最后一个城市和出发城市是否可到达，该条通路的代价是不是最优
+//		if (a[x[n - 1]][0] != NoEdge
+//			&& ((cost + a[x[n - 1]][0]) < bestc || bestc == NoEdge))
+//		{
+//			for (int i = 0; i < n; i++)
+//			{
+//				bestx[i] = x[i];
+//				bestc = cost + a[x[n - 1]][0];
+//			}
+//		}
+//	}
+//	else
+//	{
+//		for (int i = 0; i < n; i++)
+//		{
+//			//当前城市是否已经走过，当前城市与上一座城市之间是否连通
+//			if (!isHave(i, t) && a[x[t - 1]][i] != NoEdge && ((cost + a[x[t - 1]][i]) < bestc || bestc == NoEdge))
+//			{
+//				cost += a[x[t - 1]][i];
+//				//cout<<t<<endl;
+//				x[t] = i;
+//				Backtrack(t + 1);
+//				cost -= a[x[t - 1]][i];
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	x[0] = 0; //出发城市固定为第一座城市
+//	Backtrack(1);
+//	for (int i = 0; i < n; i++)
+//		cout << bestx[i] + 1 << " ";
+//	cout << endl;
+//	cout << bestc << endl;
+//	system("pause");
+//	return 0;
+//}
 //#define _CRT_SECURE_NO_WARNINGS 1
 //
 //int atoi(char* str)
