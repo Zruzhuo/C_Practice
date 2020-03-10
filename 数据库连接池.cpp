1500d9f4b0083caeb4016047c9d275118447403d
Zruzhuo <1197736628@qq.com>
2020-03-10 23:54:47 +0800

    上传练习

---
+++ 数据库连接池.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		int con = 0;
+		queue<char> q;
+		for (int i = 0; i < n; i++)
+		{
+			char ID;
+			cin >> ID;
+			string OP;
+			cin >> OP;
+			if (OP == "connect")
+				q.push(ID);
+			else
+			{
+				con = q.size() > con ? q.size() : con;
+				q.pop();
+			}
+		}
+		cout << con << endl;
+	}
+	return 0;
+}
