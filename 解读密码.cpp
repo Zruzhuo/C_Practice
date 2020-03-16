f5a7954072c23f2bc7c5a290e7b41b63a5188b4d
Zruzhuo <1197736628@qq.com>
2020-03-16 20:52:05 +0800

    上传练习

---
+++ 解读密码.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string pw;
+//	while (getline(cin, pw)) {
+//		string::iterator it = pw.begin();
+//		while (it != pw.end()) {
+//			if (*it < '0' || *it > '9') {
+//				pw.erase(it);
+//			}
+//			else {
+//				++it;
+//			}
+//		}
+//		cout << pw << endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
