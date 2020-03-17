3c64ac9e9c2ca43d14466f1ce83b15422bfdea66
Zruzhuo <1197736628@qq.com>
2020-03-17 19:56:20 +0800

    上传练习

---
+++ 最长公共子序列.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//const int MAX = 1005;
+//int dp[MAX][MAX];
+//int main()
+//{
+//	string s1, s2;
+//	memset(dp, 0, sizeof(dp));
+//	while (cin >> s1 >> s2) {
+//		int ret = 0;
+//		for (int i = 0; i < s1.size(); ++i) {
+//			for (int j = 0; j < s2.size(); ++i) {
+//				
+//			}
+//		}
+//		return ret;
+//	}
+//	system("pause");
+//	return 0;
+//}
